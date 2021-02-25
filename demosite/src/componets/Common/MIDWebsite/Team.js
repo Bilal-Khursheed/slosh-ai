cae2c405bf7f477f3a1980f81262d697a0cd2b29
Bilal-khursheed <bilal.khursheed@vizteck.com>
2021-02-25 18:57:12 +0500

    pic

--- demosite/src/componets/Common/MIDWebsite/Team.js
+++ demosite/src/componets/Common/MIDWebsite/Team.js
@@ -95,7 +95,7 @@ class Team extends Component {
             <div class="col-xl-3 col-sm-6 mb-5 grow">
               <div class="bg-white rounded shadow-sm py-5 px-4">
                 <img
-                  src="img/t3.jpg"
+                  src="img/t3.png"
                   alt=""
                   width="100"
                   class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
